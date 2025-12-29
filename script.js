e558e89524adeed1dabb1e77a68447377cc573fb
KPOGO Yayra Gloria <kpogoyayra0@gmail.com>
2025-12-29 22:06:25 +0000

    modification du code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //menu responsive
-var menu = document.querySelector('.menu');
-var menu_toggle = document.querySelector('.menu_toggle');
+let menu = document.querySelector('.menu');
+let menu_toggle = document.querySelector('.menu_toggle');
 
 menu_toggle.onclick = function(){
     menu_toggle.classList.toggle('active');
@@ -15,15 +15,15 @@ const p = document.querySelector('.left p');
 const a = document.querySelector('.left a');
 const img = document.querySelector('.bird');
 
-window.addEventListener('load',()=>{
+window.addEventListener('load', ()=>{
 
     const TL = gsap.timeline({paused: true});
     TL
-    .staggerForm(header , 2, {y:-100 , opacity:0, ease: 'power2.out'}, 0.1)
-    .staggerForm(img , 1, {x:1000, opacity:0, ease: 'power2.out'}, 0.4)
-    .staggerForm(title_span , 1, {opacity:0, ease: 'power2.out'}, 0.1)
-    .staggerForm(p, 1,{opacity:0, ease: 'power2.out'}, 0.2)
-    .staggerForm(a, 1, {opacity:0, ease: 'power2.out'}, 0.3)
+    .staggerFrom(header , 2, {y:-100 , opacity:0, ease: 'power2.out'}, 0.1)
+    .staggerFrom(img , 1, {x:1000, opacity:0, ease: 'power2.out'}, 0.4)
+    .staggerFrom(title_span , 1, {opacity:0, ease: 'power2.out'}, 0.1)
+    .staggerFrom(p, 1,{opacity:0, ease: 'power2.out'}, 0.2)
+    .staggerFrom(a, 1, {opacity:0, ease: 'power2.out'}, 0.3)
 
     TL.play()
 })
