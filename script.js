5714dddce0044b1fff44c4d4c877029dad4ff3b8
KPOGO Yayra Gloria <kpogoyayra0@gmail.com>
2025-12-29 21:49:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//menu responsive
+var menu = document.querySelector('.menu');
+var menu_toggle = document.querySelector('.menu_toggle');
+
+menu_toggle.onclick = function(){
+    menu_toggle.classList.toggle('active');
+    menu.classList.toggle('responsive');
+}
+
+//animation du site
+
+const header = document.querySelector('header');
+const title_span = document.querySelector('.left h1 span');
+const p = document.querySelector('.left p');
+const a = document.querySelector('.left a');
+const img = document.querySelector('.bird');
+
+window.addEventListener('load',()=>{
+
+    const TL = gsap.timeline({paused: true});
+    TL
+    .staggerForm(header , 2, {y:-100 , opacity:0, ease: 'power2.out'}, 0.1)
+    .staggerForm(img , 1, {x:1000, opacity:0, ease: 'power2.out'}, 0.4)
+    .staggerForm(title_span , 1, {opacity:0, ease: 'power2.out'}, 0.1)
+    .staggerForm(p, 1,{opacity:0, ease: 'power2.out'}, 0.2)
+    .staggerForm(a, 1, {opacity:0, ease: 'power2.out'}, 0.3)
+
+    TL.play()
+})
